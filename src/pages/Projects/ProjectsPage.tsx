d91782dab7e2a6356c544a8d49d3501b39d2f349
Mattias Holm Andersen <mattiasha23@gmail.com>
2026-01-23 18:24:21 +0100

    refactor: remove unused import

--- src/pages/Projects/ProjectsPage.tsx
+++ src/pages/Projects/ProjectsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation, useNavigationType } from 'react-router-dom';
 import { z } from 'zod';
 import { ProjectSchema } from '../../schemas/projectSchema';
